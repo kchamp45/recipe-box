51816ce76dfa80b64df8c52cb72c2cc1f8bedd9e
Esti Shay and Kimberly Lu <kimberly@>
2017-09-19 16:45:46 -0700

    commit at end of class

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,24 +7,21 @@ import { Component } from '@angular/core';
     <h1>Our Delicious Recipes</h1>
     <h3>Category: {{foodType}}</h3>
     <ul>
-      <li *ngFor="let eachRecipe of recipes">{{eachRecipe.title}}:
-        <ul>
-          <li>Ingredients:
-            <ul>
-              <li *ngFor="let eachIng of eachRecipe.ingredients">{{eachIng}}</li>
-            </ul>
-          </li>
-        </ul>
-        <ul>
-          <li>Directions:
-            <ol>
-              <li *ngFor="let eachStep of eachRecipe.directions">{{eachStep}}</li>
-            </ol>
-          </li>
-        </ul>
-      </li>
+      <li (click)="isTried(eachRecipe)" *ngFor="let eachRecipe of recipes">{{eachRecipe.title}} <button (click)= "editRecipe(eachRecipe)">Edit</button></li>:
     </ul>
-
+    <hr>
+    <div>
+      <p>Tried recipe? {{selectedRecipe.tried}}</p>
+      <h3>Edit recipe</h3>
+      <div>
+      <label>Enter Ingredients:</label>
+      <input [(ngModel)]="selectedRecipe.ingredients">
+      </div>
+      <div>
+      <label>Enter directions:</label>
+      <input [(ngModel)]="selectedRecipe.directions">
+    </div>
+  </div>
   </div>
   `
 })
@@ -37,9 +34,20 @@ export class AppComponent{
     new Recipe('Banana Wonton', ['Bananas', 'Wonton wraps', 'cinnamon', 'sugar'], ['Wrap banana in wonton. Deep fry for 3 min.']),
     new Recipe('Baklava', ['Filo sheets', 'honey', 'walnuts', 'pistachios'], ['Magic'])
   ];
+  selectedRecipe: Recipe = this.recipes[0];
+  // selectedRecipe = null;
 
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
 
-
+  isTried(clickedRecipe: Recipe) {
+    // if(clickedRecipe.tried === true) {
+    //   alert("This recipe has been tried!");
+    // }else{
+    //   alert("This recipe has not been tried.");
+    // }
+  }
 }
 
 export class Recipe {
@@ -47,3 +55,18 @@ export class Recipe {
   constructor(public title: string, public ingredients: string[], public directions: string[]) { }
 }
 //Array<string>
+// <li>Ingredients:
+//   <ul>
+//     <li *ngFor="let eachIng of eachRecipe.ingredients">{{eachIng}}</li>
+//   </ul>
+// </li>
+// </ul>
+// <ul>
+// <li>Directions:
+//   <ol>
+//     <li *ngFor="let eachStep of eachRecipe.directions">{{eachStep}}</li>
+//   </ol>
+// </li>
+// </ul>
+// </li>
+// </ul>
