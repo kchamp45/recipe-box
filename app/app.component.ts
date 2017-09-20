61c6631296734108f7e3b935a99a002501b91fd8
Kimberly Lu <kimlu4@comcast.net>
2017-09-19 23:02:33 -0700

    add logics to app.component to display details of recipe and edit recipes.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,21 +7,39 @@ import { Component } from '@angular/core';
     <h1>Our Delicious Recipes</h1>
     <h3>Category: {{foodType}}</h3>
     <ul>
-      <li (click)="isTried(eachRecipe)" *ngFor="let eachRecipe of recipes">{{eachRecipe.title}} <button (click)= "editRecipe(eachRecipe)">Edit</button></li>:
+      <li (click)="isTried(eachRecipe)" *ngFor="let eachRecipe of recipes">{{eachRecipe.title}} <button (click)= "editRecipe(eachRecipe)">Edit</button> <button (click)="display(eachRecipe)">Details</button></li>:
     </ul>
     <hr>
-    <div>
+    <div *ngIf="selectedRecipe">
       <p>Tried recipe? {{selectedRecipe.tried}}</p>
       <h3>Edit recipe</h3>
-      <div>
-      <label>Enter Ingredients:</label>
-      <input [(ngModel)]="selectedRecipe.ingredients">
+      <div >
+        <label>Enter new title:</label>
+        <input [(ngModel)]="selectedRecipe.title">
+      </div>
+      <div >
+        <label>Edit ingredients:</label>
+        <input [(ngModel)]="selectedRecipe.ingredients">
       </div>
       <div>
-      <label>Enter directions:</label>
-      <input [(ngModel)]="selectedRecipe.directions">
+        <label>Edit directions:</label>
+        <input [(ngModel)]="selectedRecipe.directions"><br>
+        <button (click)="finishedEditing()">Done!</button>
+      </div>
     </div>
-  </div>
+    <div *ngIf="aRecipe">
+      <li>Ingredients:
+       <ul>
+        <li>{{aRecipe.ingredients}}</li>
+       </ul>
+     </li>
+     <li>Directions:
+       <ul>
+         <li>{{aRecipe.directions}}</li>
+       </ul>
+     </li>
+     <button (click)="finishedLooking()">Got it!</button>
+    </div>
   </div>
   `
 })
@@ -30,15 +48,28 @@ export class AppComponent{
   foodType: string = 'Desserts';
   recipes: Recipe[] = [
     new Recipe('Banana Bread', ['Bananas', 'Sugar', 'Flour', 'Sour Cream', 'Love'], ['Preheat oven', 'Mix fats', 'Mix dry ingredients', 'Mash bananas', 'Mix all until smooth', 'Bake a while']),
-    new Recipe('Chocolate Chip Cookies', ['Oatmeal flour', 'sugar', 'butter', 'semi-sweet chocolate chips', 'vanilla'], ["Bake at 350 degrees F whatever dough the kids don't eat"]),
-    new Recipe('Banana Wonton', ['Bananas', 'Wonton wraps', 'cinnamon', 'sugar'], ['Wrap banana in wonton. Deep fry for 3 min.']),
-    new Recipe('Baklava', ['Filo sheets', 'honey', 'walnuts', 'pistachios'], ['Magic'])
+    new Recipe('Chocolate Chip Cookies', ['Oatmeal flour', 'sugar', 'butter', 'semi-sweet chocolate chips', 'vanilla'], ['Mix dry ingredients', 'Add in soften butter', 'Stir in chocolate chips', 'Bake at 350 degrees F for 10 minutes']),
+    new Recipe('Banana Wonton', ['Bananas', 'Wonton wraps', 'cinnamon', 'sugar'], ['Wrap banana in wonton', 'Deep fry for 3 min.', 'Sprinkle sugar and cinnamon over the wontons']),
+    new Recipe('Baklava', ['Filo sheets', 'honey', 'walnuts', 'pistachios'], ['Mix honey and nuts together', 'Lay down 2 filo sheets', 'Spread a thin layer of honey nut mixture over the sheets', 'Repeat steps 2 and 3', 'Finish over with a layer of filo sheets', 'Bake at 350 degrees F for 30 mins.'])
   ];
-  selectedRecipe: Recipe = this.recipes[0];
-  // selectedRecipe = null;
+  selectedRecipe = null;
+  aRecipe = null;
+
 
   editRecipe(clickedRecipe) {
     this.selectedRecipe = clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
+  display(theRecipe) {
+    this.aRecipe = theRecipe;
+  }
+
+  finishedLooking() {
+    this.aRecipe = null;
   }
 
   isTried(clickedRecipe: Recipe) {
@@ -48,25 +79,10 @@ export class AppComponent{
     //   alert("This recipe has not been tried.");
     // }
   }
+  
 }
 
 export class Recipe {
   public tried: boolean = false;
   constructor(public title: string, public ingredients: string[], public directions: string[]) { }
 }
-//Array<string>
-// <li>Ingredients:
-//   <ul>
-//     <li *ngFor="let eachIng of eachRecipe.ingredients">{{eachIng}}</li>
-//   </ul>
-// </li>
-// </ul>
-// <ul>
-// <li>Directions:
-//   <ol>
-//     <li *ngFor="let eachStep of eachRecipe.directions">{{eachStep}}</li>
-//   </ol>
-// </li>
-// </ul>
-// </li>
-// </ul>
